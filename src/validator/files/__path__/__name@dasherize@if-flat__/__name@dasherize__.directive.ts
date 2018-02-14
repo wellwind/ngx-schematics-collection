f5d1cfa4e5161e1717882cced21ca4b5c7725332
wellwind <wellwind@mail2000.com.tw>
2018-02-14 21:16:30 +0800

    Support async validator

--- src/validator/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.directive.ts
+++ src/validator/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.directive.ts
@@ -1,17 +1,23 @@
 import { Directive } from '@angular/core';
+import { AbstractControl, ValidationErrors, NG_<% if(isAsync) { %>ASYNC_<% } %>VALIDATORS, <% if(isAsync) { %>Async<% } %>Validator } from '@angular/forms';
+<% if(isAsync) { %>import { Observable } from 'rxjs/Observable';<% } %>
 
 @Directive({
   selector: '[<%= selector %>]',
-  providers: [{ provide: NG_VALIDATORS, useExisting: <%= classify(name) %>Directive, multi: true }]
+  providers: [{ provide: NG_<% if(isAsync) { %>ASYNC_<% } %>VALIDATORS, useExisting: <%= classify(name) %>Directive, multi: true }]
 })
-export class <%= classify(name) %>Directive implements Validator {
+export class <%= classify(name) %>Directive implements <% if(isAsync) { %>Async<% } %>Validator {
 
   constructor() {}
-
-  validate(c: AbstractControl): { [key: string]: any } {
-    return;
+<% if(isAsync) { %>
+  validate(c: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    return null;
   }
-
+<% } else { %>
+  validate(c: AbstractControl): ValidationErrors | null {
+    return null;
+  }
+<% } %>
   registerOnValidatorChange?(fn: () => void): void {
     throw new Error('Method not implemented.');
   }
