36ab6f2ffe63e36a2f7c302a558c3e27d742ee28
wellwind <wellwind@mail2000.com.tw>
2018-02-15 17:37:58 +0800

    Add Resolve route guard

--- src/route-guard/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.service.ts
+++ src/route-guard/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.service.ts
@@ -4,7 +4,6 @@ import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class <%= classify(name) %>Service implements CanActivate {
-
   constructor() {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
@@ -16,14 +15,12 @@ import { Observable } from 'rxjs/Observable';
   
 @Injectable()
 export class <%= classify(name) %>Service implements CanActivateChild {
-
   constructor() {}
 
   canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
     throw new Error('Method not implemented.');
   }
-}  
-<% } else if(type === 'CanDeactivate') { %>import { Injectable } from '@angular/core';
+}<% } else if(type === 'CanDeactivate') { %>import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
@@ -39,5 +36,15 @@ export class <%= classify(name) %>Service implements CanDeactivate<any> {
   ): boolean | Observable<boolean> | Promise<boolean> {
     return true;
   }
-}
-<% } %>
+}<% } else if(type === 'Resolve') { %>import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class <%= classify(name) %>Service implements Resolve<any> {
+  constructor() {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+    return null;
+  }
+}<% } %>
