adae8fb86ed4af46e3820f66c8caddd5b4b1a468
wellwind <wellwind@mail2000.com.tw>
2018-02-15 17:28:47 +0800

    Add CanActivateChild route guard

--- src/route-guard/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.service.ts
+++ src/route-guard/files/__path__/__name@dasherize@if-flat__/__name@dasherize__.service.ts
@@ -1,13 +1,26 @@
-<% if(type === 'CanActivate') { %>
-import { Injectable } from '@angular/core';
+<% if(type === 'CanActivate') { %>import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class <%= classify(name) %>Service implements CanActivate {
+
   constructor() {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
     return true;
   }
-}<% } %>
+}<% } else if(type === 'CanActivateChild') { %>import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateChild, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+  
+@Injectable()
+export class Activate3Service implements CanActivateChild {
+
+  constructor() {}
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    throw new Error('Method not implemented.');
+  }
+}  
+<% } %>
